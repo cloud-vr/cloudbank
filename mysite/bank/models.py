03832130ef25a71b51aa9ea20800c8a8ae763431
cloud-vr <gvsreyes@gmail.com>
2020-04-18 21:59:31 +0800

    added logic for updating balance. covers deposit, withdraw, transfer transactions.

--- mysite/bank/models.py
+++ mysite/bank/models.py
@@ -11,6 +11,7 @@ class Client(models.Model):
     acct_num = models.IntegerField('Account Number')
     mobile_num = models.IntegerField('Mobile Number')
     email_addr = models.EmailField('Email Address', max_length=200)
+    balance = models.FloatField('Account Balance')
     # Foreign Key
     created_by = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
@@ -32,7 +33,7 @@ class DepositTransaction(models.Model):
     client = models.ForeignKey(Client, default=None, on_delete=models.CASCADE)
     # Transaction Computation
     deposit_amt = models.FloatField('Deposit Amount')
-    curr = models.CharField('Currency', max_length=20)
+    curr = models.CharField('Currency', default='PHP', max_length=20)
     current_balance = models.FloatField('Current Balance')
     total_balance = models.FloatField('Total Balance')
 
@@ -53,10 +54,28 @@ class WithdrawTransaction(models.Model):
     # Client Information
     client = models.ForeignKey(Client, default=None, on_delete=models.CASCADE)
     # Transaction Computation
-    withdraw_amt = models.FloatField('Deposit Amount')
-    curr = models.CharField('Currency', max_length=20)
+    withdraw_amt = models.FloatField('Withdraw Amount')
+    curr = models.CharField('Currency', default='PHP', max_length=20)
     current_balance = models.FloatField('Current Balance')
     total_balance = models.FloatField('Total Balance')
 
     def __str__(self):
         return self.trx_ref
+
+
+class TransferTransaction(models.Model):
+    # General Transaction Information
+    trx_date = models.DateField('Date', auto_now_add=True)
+    trx_ref = models.CharField(
+        max_length=10,
+        default=str(random.randint(1000000000, 9999999999))
+    )
+    status = models.CharField('Status', max_length=50)
+    # Foreign Key
+    created_by = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    # Client Information
+    from_client = models.ForeignKey(Client, related_name='from_client', default=None, on_delete=models.CASCADE)
+    to_client = models.ForeignKey(Client, related_name='to_client', default=None, on_delete=models.CASCADE)
+    # Transaction Computation
+    transfer_amt = models.FloatField('Transfer Amount')
+    curr = models.CharField('Currency', default='PHP', max_length=20)

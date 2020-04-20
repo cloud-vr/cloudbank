f8f2c2fceb9789b9a53986e21104483c8c45fdcd
cloud-vr <gvsreyes@gmail.com>
2020-04-20 21:56:27 +0800

    view added for client, deposit, withdraw, transfer transactions.

--- mysite/bank/urls.py
+++ mysite/bank/urls.py
@@ -7,10 +7,14 @@ app_name = 'bank'
 urlpatterns = [
     path('application_list', views.application_list, name='application_list'),
     path('client_accounts', views.client_accounts, name='client_accounts'),
+    path('view_client', views.view_client, name='view_client'),
     path('create_client', views.create_client, name='create_client'),
     path('edit_client', views.edit_client, name='edit_client'),
     path('delete_client', views.delete_client, name='delete_client'),
+    path('view_deposit_trx', views.view_deposit_trx, name='view_deposit_trx'),
     path('create_deposit_trx', views.create_deposit_trx, name='create_deposit_trx'),
+    path('view_withdraw_trx', views.view_withdraw_trx, name='view_withdraw_trx'),
     path('create_withdraw_trx', views.create_withdraw_trx, name='create_withdraw_trx'),
+    path('view_transfer_trx', views.view_transfer_trx, name='view_transfer_trx'),
     path('create_transfer_trx', views.create_transfer_trx, name='create_transfer_trx')
 ]

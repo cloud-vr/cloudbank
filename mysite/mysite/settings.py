fa5ec30dedd93b2139c5bd1d9c03c6828b9dd5a9
cloud-vr <gvsreyes@gmail.com>
2020-05-02 16:13:31 +0800

    finalized bootstrap design

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '8skh4ihy#vo(zzm9fzovgip9$q_$75s=kbsbkcjd8_%5q%#j=7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 

7d31a0682b2a789336aeecc5169a57f25ce21b11
cloud-vr <gvsreyes@gmail.com>
2020-05-16 18:28:52 +0800

    added red display for errors in - object_form.html - object_list.html - login.html
    updated client model to be have unique email_addr and
    account number
    
    replace isinstance with isdigit as the former does not work.

--- mysite/bank/models.py
+++ mysite/bank/models.py
@@ -8,9 +8,9 @@ class Client(models.Model):
     fname = models.CharField('First Name', max_length=20)
     lname = models.CharField('Last Name', max_length=20)
     addr = models.CharField('Address', max_length=50)
-    acct_num = models.IntegerField('Account Number')
+    acct_num = models.IntegerField('Account Number', unique=True)
     mobile_num = models.IntegerField('Mobile Number')
-    email_addr = models.EmailField('Email Address', max_length=200)
+    email_addr = models.EmailField('Email Address', max_length=200, unique=True)
     balance = models.FloatField('Account Balance')
     # Foreign Key
     created_by = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
